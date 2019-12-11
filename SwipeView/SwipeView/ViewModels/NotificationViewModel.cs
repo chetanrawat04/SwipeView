9bf1184c7e060f74ba88bcdabb104c79e621c42d
chetan rawat <chetan.rawat@hicom.in>
2019-12-11 17:01:59 +0530

    Initial Commit

---
+++ SwipeView/SwipeView/ViewModels/NotificationViewModel.cs
@@ -0,0 +1,47 @@
+﻿using SwipeView.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SwipeView.ViewModels
+{
+    class NotificationViewModel : INotifyPropertyChanged
+    {
+        ObservableCollection<NotificationModel> _Items;
+        public ObservableCollection<NotificationModel> Items
+        {
+            get
+            {
+                return _Items;
+            }
+            set
+            {
+                _Items = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public NotificationViewModel()
+        {
+            Items = new ObservableCollection<NotificationModel>();
+            AddItems();
+        }
+        void AddItems()
+        {
+            _Items.Add(new NotificationModel { Title = "Info", Message = "This is only information message please ignor this one." });
+            _Items.Add(new NotificationModel { Title = "Alert", Message = "This is only Alert message please ignor this one." });
+            _Items.Add(new NotificationModel { Title = "Suggesstion", Message = "This is only Suggesstion message please ignor this one." });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ SwipeView/SwipeView/Views/NotificationPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SwipeView.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NotificationPage : ContentPage
+	{
+		public NotificationPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void SwipeItem_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SwipeItem_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
